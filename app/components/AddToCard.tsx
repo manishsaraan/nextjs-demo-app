50bd473d3e64c7a815c35a4a4a0a464d730d7103
manishsaraan <manish021js@gmail.com>
2023-10-23 17:43:25 +0530

    initial commit

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={() => console.log("sdfeeeeresdfd")}>add wwwwwwwwwto cart</button>
+  )
+}
+
+export default AddToCard
